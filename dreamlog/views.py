593060fae3e1c5e89ee9588eecdd9e17aa7f80eb
cuimuxi <cuimuxi@gmail.com>
2012-12-21 14:01:53 +0800

    modiy router

--- dreamlog/views.py
+++ dreamlog/views.py
@@ -1,6 +1,11 @@
+#encoding=utf-8
 from pyramid.view import view_config
-
+from pyramid.response import Response
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'dreamLog'}
+
+@view_config(route_name='echo')
+def echo(request):
+    return Response(request.matchdict['var'])
